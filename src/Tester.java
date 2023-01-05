338472bf24394112ac46c12f43e967d25717f6eb
Elad <eladv@mail.tau.ac.il>
2023-01-05 17:05:18 +0200

    changed tester to question 1 in the experimental part program

--- src/Tester.java
+++ src/Tester.java
@@ -1,19 +1,33 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Tester {
 
 
 
     public static void main(String[] args) {
-
         FibonacciHeap heap = new FibonacciHeap();
-        for (int i = 0; i < 4; i++) {
-            heap.insert(i);
+        double start = System.currentTimeMillis();
+        int m = (int) Math.pow(2, 20);
+        Map<Integer, FibonacciHeap.HeapNode> nodes = new HashMap<>();
+        for (int k = m - 1; k >  -2; k--) {
+            nodes.put(k, heap.insert(k));
         }
-        FibonacciHeap.HeapNode x = heap.insert(4);
         heap.deleteMin();
-        heap.delete(x);
-        FibonacciHeap.HeapNode node = heap.getFirst().getChild().getNext();
+        int size = (int)Math.ceil(Math.log(m) / Math.log(2));
+        for (int i = size; i > 0 ; i--) {
+            heap.decreaseKey(nodes.get(m - (int)Math.pow(2, i) + 1), m + 1);
+        }
+
+        double end = System.currentTimeMillis();
+
+        System.out.println("time: " + (end- start));
+        System.out.println("links: " + FibonacciHeap.totalLinks());
+        System.out.println("cuts: " + FibonacciHeap.totalCuts());
+        System.out.println("potential: " + heap.potential());
+
+
 
     }
 
