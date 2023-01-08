4d12337b9b9483815fa8d9c23e5ac4305a08164f
Elad <eladv@mail.tau.ac.il>
2023-01-08 18:24:02 +0200

    tester

--- src/Tester.java
+++ src/Tester.java
@@ -8,25 +8,26 @@ public class Tester {
 
     public static void main(String[] args) {
         FibonacciHeap heap = new FibonacciHeap();
-        double start = System.currentTimeMillis();
-        int m = (int) Math.pow(2, 20);
-        Map<Integer, FibonacciHeap.HeapNode> nodes = new HashMap<>();
-        for (int k = m - 1; k >  -2; k--) {
-            nodes.put(k, heap.insert(k));
+        for (int i = 5; i <= 20; i += 5) {
+            double start = System.currentTimeMillis();
+            int m = (int) Math.pow(2, i);
+            Map<Integer, FibonacciHeap.HeapNode> nodes = new HashMap<>();
+            for (int k = m - 1; k > -2; k--) {
+                nodes.put(k, heap.insert(k));
+            }
+            heap.deleteMin();
+            int size = (int) Math.ceil(Math.log(m) / Math.log(2));
+            for (int j = size; j > 0; j--) {
+                heap.decreaseKey(nodes.get(m - (int) Math.pow(2, j) + 1), m + 1);
+            }
+
+            double end = System.currentTimeMillis();
+            System.out.println("m = 2^" + i);
+            System.out.println("time: " + (end - start));
+            System.out.println("links: " + FibonacciHeap.totalLinks());
+            System.out.println("cuts: " + FibonacciHeap.totalCuts());
+            System.out.println("potential: " + heap.potential());
         }
-        heap.deleteMin();
-        int size = (int)Math.ceil(Math.log(m) / Math.log(2));
-        for (int i = size; i > 0 ; i--) {
-            heap.decreaseKey(nodes.get(m - (int)Math.pow(2, i) + 1), m + 1);
-        }
-
-        double end = System.currentTimeMillis();
-
-        System.out.println("time: " + (end- start));
-        System.out.println("links: " + FibonacciHeap.totalLinks());
-        System.out.println("cuts: " + FibonacciHeap.totalCuts());
-        System.out.println("potential: " + heap.potential());
-
 
 
     }
